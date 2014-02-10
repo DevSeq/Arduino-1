7b69778b5c9a750c8ca20117c61d056cc7609aa1
Ali Lown <ali@lown.me.uk>
2014-02-10 17:01:56 +0000

    Fix main to run the scheduler

--- hardware/arduino/cores/oscar/main.cpp
+++ hardware/arduino/cores/oscar/main.cpp
@@ -1,20 +1,33 @@
 #include <Arduino.h>
+#include <config.h>
+#include <wdt_drv.h>
+#include <power_drv.h>
 
+//Main entry point for OSCAR.
+//Starts the scheduler, and then runs threads afterwards
+//Check out device_template_task for the Arduino-side threads
 int main(void)
 {
-	init();
-
-#if defined(USBCON)
-	USBDevice.attach();
-#endif
-	
-	setup();
-    
-	for (;;) {
-		loop();
-		if (serialEventRun) serialEventRun();
-	}
-        
-	return 0;
+  wdtdrv_disable();
+  Clear_prescaler();
+  scheduler();
+  return 0;
 }
 
+//Set up a lower-level initialization function
+//Increment the CPU clock before RAM initialization
+#ifdef __GNUC__
+char __low_level_init(void) __atribute__ ((section(".init3"), naked));
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+char __low_level_init()
+{
+  Clear_prescaler();
+  return 1;
+}
+#ifdef __cplusplus
+}
+#endif
